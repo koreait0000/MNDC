8ec0d03ead08db5377b1f86a7a7e75d38c45c11a
Kimsw-G <kimsw3445@naver.com>
2021-05-31 16:29:50 +0900

    change config make vo make package make configFile

---
+++ src/main/java/com/example/mndc/controller/BoardVO.java
@@ -0,0 +1,19 @@
+package com.example.mndc.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardVO {
+    private int b_pk;
+    private String b_title;
+    private String b_ctnt;
+    private String b_regdt;
+    private int b_like;
+    private int b_view;
+    private int b_type;
+    private int m_pk;
+    private int mu_pk;
+
+}
