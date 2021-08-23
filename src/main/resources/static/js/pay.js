580248c5cb81d1d4532ce898c500612950e7159f
hyung000620 <dksms1@naver.com>
2021-08-23 09:11:08 +0900

    구독 info 페이지

--- src/main/resources/static/js/pay.js
+++ src/main/resources/static/js/pay.js
@@ -1,12 +1,55 @@
 const tossPayments = TossPayments("test_ck_JQbgMGZzorzzXdypGB7rl5E1em4d");
-const button = document.getElementById("payment-button");
+const button1 = document.getElementById("payment-button1");
+const button2 = document.getElementById("payment-button2");
+const button3 = document.getElementById("payment-button3");
+const button4 = document.getElementById("payment-button4");
 const orderId = new Date().getTime();
-button.addEventListener("click", function () {
+
+button1.addEventListener("click", function () {
     let paymentData = {
-        amount: 19000,
+        amount: 1000,
         orderId: orderId,
-        orderName: " ",//상품명
-        customerName: " ", //고객이름
+        orderName: "이등병",//상품명
+        customerName: "고객 이름", //고객이름
+        successUrl: window.location.origin + "/success",
+        failUrl: window.location.origin + "/fail",
+    };
+
+    tossPayments.requestPayment(paymentData);
+});
+
+button2.addEventListener("click", function () {
+    let paymentData = {
+        amount: 2000,
+        orderId: orderId,
+        orderName: "일병",//상품명
+        customerName: "고객 이름", //고객이름
+        successUrl: window.location.origin + "/success",
+        failUrl: window.location.origin + "/fail",
+    };
+
+    tossPayments.requestPayment(paymentData);
+});
+
+button3.addEventListener("click", function () {
+    let paymentData = {
+        amount: 3000,
+        orderId: orderId,
+        orderName: "상병",//상품명
+        customerName: "고객 이름", //고객이름
+        successUrl: window.location.origin + "/success",
+        failUrl: window.location.origin + "/fail",
+    };
+
+    tossPayments.requestPayment(paymentData);
+});
+
+button4.addEventListener("click", function () {
+    let paymentData = {
+        amount: 4000,
+        orderId: orderId,
+        orderName: "병장",//상품명
+        customerName: "고객 이름", //고객이름
         successUrl: window.location.origin + "/success",
         failUrl: window.location.origin + "/fail",
     };
--- src/main/webapp/WEB-INF/views/subscription/info.jsp
+++ src/main/webapp/WEB-INF/views/subscription/info.jsp
@@ -1,3 +1,28 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--TODO: pay는 연결했는데. 구매했을때 혜택? 그리고 가격? 다른데 보고 고민해보고 해야할듯?--%>
+
+<link rel="stylesheet" href="/css/info.css">
+
+<div class="img-container-box">
+    <div class="img-container">
+        <div><img src="/img/이등병.jpg"></div>
+        <button id="payment-button1" class="pay-btn">1000</button>
+    </div>
+    <div class="img-container">
+        <div><img src="/img/일병.jpg"></div>
+        <button id="payment-button2" class="pay-btn">2000</button>
+    </div>
+    <div class="img-container">
+        <div><img src="/img/상병.jpg"></div>
+        <button id="payment-button3" class="pay-btn">3000</button>
+    </div>
+    <div class="img-container">
+        <div><img src="/img/병장.jpg"></div>
+        <button id="payment-button4" class="pay-btn">4000</button>
+    </div>
+</div>
+<div>구매 시 위 뱃지가 수여됩니다.</div>
+<script src="https://js.tosspayments.com/v1"></script>
+<script src="/js/pay.js"></script>
